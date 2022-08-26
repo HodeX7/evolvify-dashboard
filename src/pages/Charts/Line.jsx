41440d3099fdb9a7a4c256e2919f294f9113dceb
Akshar Mehta <aksharmehta@Akshars-MacBook-Air.local>
2022-08-26 18:16:06 +0530

    EVOLVIFY DASHBOARD

--- src/pages/Charts/Line.jsx
+++ src/pages/Charts/Line.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from "react";
 
-import { ChartsHeader, LineChart } from '../../components';
+import { ChartsHeader, LineChart } from "../../components";
 
 const Line = () => (
   <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
-    <ChartsHeader category="Line" title="Inflation Rate" />
+    <ChartsHeader category="Line" title="Line Chart on various Quotients" />
     <div className="w-full">
       <LineChart />
     </div>
